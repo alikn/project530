7737bc285a4e1e39c796bc11c3ad237b1f08f311
yasmin f <yasminfaisals@gmail.com>
2015-04-05 22:43:45 -0700

    fixing y axis label for bar chart #43

--- scripts/barChart.js
+++ scripts/barChart.js
@@ -75,11 +75,11 @@ define(["dataProcess","d3"], function (dataProcess,d3) {
 		        .attr("class", "y axis")
 		        .call(yAxis)
 		    .append("text")
-		        .attr("transform", "rotate(-90)")
-		        .attr("y", -60)
-		        .attr("dy", ".71em")
-		        .style("text-anchor", "end")
-		        .text("Tonnes");
+                .attr("class", "axis-label")
+                .attr("transform", "rotate(-90)")
+                .attr("y", (-margin.left) + 10)
+                .attr("x", -height)
+                .text('Tonnes');
 
 		svg.selectAll(".barsuccess")
 		        .data(d3.entries(matEmissions))
